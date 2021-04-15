78c73bbc3b0a3d445ed44ff70e0246c453aaef67
satht <satht2@yahoo.com>
2021-04-14 22:03:58 -0400

    Death certificate

--- Parish/Controls/addCertificateOfDeath.ascx
+++ Parish/Controls/addCertificateOfDeath.ascx
@@ -1,1 +1,104 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="addCertificateOfDeath.ascx.cs" Inherits="Controls_addCertificateOfDeath" %>
+
+
+<script>
+    $(document).ready(function () {
+        $("#<%= txtDODeath.ClientID %>").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            maxDate: "+0M + 0D",
+            dateFormat: 'd-m-yy'
+        });
+        $("#<%= txtDOBurial.ClientID %>").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            maxDate: "+0M + 0D",
+            dateFormat: 'd-m-yy'
+        });
+    });
+
+</script>
+<div>
+    <table align="center" class="content_border">
+        <tr>
+            <td colspan="4">
+                <h2>Add Certificate of Death</h2>
+                <asp:Label ID="lblError" ForeColor="Red" Font-Bold="true" runat="server" Text=""></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 150px;">Name:
+            </td>
+            <td>
+                <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            </td>
+            <td style="width: 30px;">&nbsp;
+            </td>
+            <td>Sex:
+            </td>
+            <td>
+                <asp:DropDownList ID="ddSex" runat="server">
+                </asp:DropDownList>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 150px;">Age:
+            </td>
+            <td>
+                <asp:TextBox ID="txtAge" runat="server"></asp:TextBox>
+            </td>
+            <td style="width: 30px;">&nbsp;
+            </td>
+            <td style="width: 130px;">Address:
+            </td>
+            <td>
+                <asp:TextBox ID="txtAddress" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 150px;">Date of Death(d-m-yy):
+            </td>
+            <td>
+                <asp:TextBox ID="txtDODeath" runat="server"></asp:TextBox>
+            </td>
+            <td style="width: 30px;">&nbsp;
+            </td>
+            <td style="width: 130px;">Date of Burial(d-m-yy):
+            </td>
+            <td>
+                <asp:TextBox ID="txtDOBurial" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>Place of Burial:
+            </td>
+            <td>
+                <asp:TextBox ID="txtPlaceOfBurial" runat="server"></asp:TextBox>
+            </td>
+            <td style="width: 30px;">&nbsp;
+            </td>
+            <td>Date of Given(d-m-yy):
+            </td>
+            <td>
+                <asp:TextBox ID="txtDOGiven" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>Parish Priest:
+            </td>
+            <td>
+                <asp:TextBox ID="txtParishPriest" runat="server"></asp:TextBox>
+            </td>
+            <td style="width: 30px;">&nbsp;
+            </td>
+            <td>Deed:
+            </td>
+            <td>
+                <asp:TextBox ID="txtDeed" runat="server"></asp:TextBox>
+            </td>
+        </tr>        
+    </table>
+    <div align="center" style="padding-top: 10px;">
+        <asp:Button CssClass="cssButton" ID="bSave" runat="server" Text="Save" OnClick="bSave_Click" />
+    </div>
+</div>
--- Parish/Controls/addCertificateOfDeath.ascx.cs
+++ Parish/Controls/addCertificateOfDeath.ascx.cs
@@ -1,14 +1,183 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class Controls_addCertificateOfDeath : System.Web.UI.UserControl
 {
+    public int MemberID { get; set; }
+    public int CertificateTypeID { get; set; }
+    protected StringBuilder msgError = new StringBuilder();
+
+    public void AddBaptism()
+    {
+        //GetSex();
+        if (MemberID != 0)
+        {
+            Session["MemberID"] = MemberID;
+            //bSave.Text = "Update";
+        }
+        else
+        {
+            //bSave.Text = "Save";
+            ClearCertificate();
+        }
+
+    }
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!Page.IsPostBack)
+        {
+            GetSex();
+            if (MemberID != 0)
+            {
+                Session["MemberID"] = MemberID;
+                bSave.Text = "Update";
+            }
+            else
+            {
+                Session["MemberID"] = null;
+                bSave.Text = "Save";
+                ClearCertificate();
+            }
+        }
+    }
+    protected void GetSex()
+    {
+        ddSex.Items.Insert(0, new ListItem("Select one"));
+        ddSex.Items.Insert(1, new ListItem("Male"));
+        ddSex.Items.Insert(2, new ListItem("Female"));
+    }
+    private void ClearCertificate()
+    {
+        txtName.Text = "";
+        txtAddress.Text = "";
+        txtAge.Text = "";
+        txtDODeath.Text = "";
+        txtDOBurial.Text = "";
+        txtPlaceOfBurial.Text = "";
+        txtDOGiven.Text = "";
+        txtDeed.Text = "";
+        txtDOGiven.Text = "";
+        txtParishPriest.Text = "";
+    }
+    protected void bSave_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            lblError.ForeColor = System.Drawing.Color.Red;
+            if (ValidateClientInfo())
+            {
+                MemberID = Convert.ToInt32(Session["MemberID"]);
+                int UserID = Convert.ToInt32(Session["UserID"]);
+                //DataClients dC = new DataClients();
 
+                //string dv = dC.InsertCertifiateOfBaptism(MemberID, 1, txtName.Text, ddSex.SelectedValue, txtParentFatherName.Text
+                //    , txtParentMotherName.Text, Convert.ToDateTime(txtDOB.Text), Convert.ToDateTime(txtDOBaptism.Text), txtSponsor1.Text
+                //    , txtSponsor2.Text, txtMinister.Text, null, null, Convert.ToDateTime(txtDOGiven.Text)
+                //    , txtParishPriest.Text, UserID);
+
+                //if (Utilities.IsNumeric(dv) && Convert.ToInt32(dv) == -1)
+                //{
+                //    lblError.Text = "DB Error.<br/>";
+                //}
+                //else
+                //{
+                //    ClearCertificate();
+                //    lblError.Text = "Success: The confirmation certificate added to the system.";
+                //    lblError.ForeColor = System.Drawing.Color.Green;
+                //}
+            }
+            else
+                lblError.Text = msgError.ToString();
+        }
+
+        catch (Exception ex)
+        {
+            lblError.Text = "Please validate your information.<br/>" + ex.Message + msgError.ToString();
+        }
+    }
+    protected Boolean ValidateClientInfo()
+    {
+        Boolean bValidate = true;
+        int currYear = DateTime.Today.Year;
+        DateTime dt = DateTime.Today;
+
+        lblError.Text = "";
+        try
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text.Trim()))
+            {
+                bValidate = false;
+                msgError.AppendFormat("Enter name.<br/>");
+            }
+            if (ddSex.SelectedIndex == 0)
+            {
+                bValidate = false;
+                msgError.AppendFormat("Please select the sex.<br/>");
+            }
+            if (string.IsNullOrWhiteSpace(txtAddress.Text.Trim()))
+            {
+                bValidate = false;
+                msgError.AppendFormat("Enter address<br/>");
+            }
+            if (string.IsNullOrWhiteSpace(txtAge.Text.Trim()))
+            {
+                bValidate = false;
+                msgError.AppendFormat("Enter age<br/>");
+            }
+            if (!Utilities.IsNumeric(txtAge.Text) && Convert.ToInt32(txtAge.Text) < 0)
+            {
+                bValidate = false;
+                msgError.AppendFormat("Please number that greater than 0<br/>");
+            }            
+            if (string.IsNullOrWhiteSpace(txtDODeath.Text.Trim()))
+            {
+                bValidate = false;
+                msgError.AppendFormat("Please select the Date of death.<br/>");
+            }
+            else if (DateTime.Parse(txtDODeath.Text) > dt)
+            {
+                bValidate = false;
+                msgError.AppendFormat("Date of death cannot be future date.<br/>");
+            }
+            if (DateTime.Parse(txtDOBurial.Text) > dt)
+            {
+                bValidate = false;
+                msgError.AppendFormat("Date of burial cannot be future date.<br/>");
+            }
+            else if(string.IsNullOrWhiteSpace(txtDOBurial.Text.Trim()))
+            {
+                bValidate = false;
+                msgError.AppendFormat("Please select the Date of burial.<br/>");
+            }                        
+            if (string.IsNullOrWhiteSpace(txtDOGiven.Text.Trim()))
+            {
+                bValidate = false;
+                msgError.AppendFormat("Please select the Date of given.<br/>");
+            }
+            else if (DateTime.Parse(txtDOGiven.Text) > dt)
+            {
+                bValidate = false;
+                msgError.AppendFormat("Date of birth cannot be future date.<br/>");
+            }
+            if (string.IsNullOrWhiteSpace(txtParishPriest.Text.Trim()))
+            {
+                bValidate = false;
+                msgError.AppendFormat("Enter parish priest name<br/>");
+            }
+            if (string.IsNullOrWhiteSpace(txtPlaceOfBurial.Text.Trim()))
+            {
+                bValidate = false;
+                msgError.AppendFormat("Enter place of burial.<br/>");
+            }
+        }
+        catch
+        {
+        }
+        return bValidate;
     }
 }
